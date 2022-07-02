d1e30041a532191001d7b8053b11423938826b56
Brian Yuen <byuen@pandora.com>
2022-07-02 10:46:50 -0700

    add OrbitControls

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import { useRef, useState } from 'react'
-import { Canvas, useFrame } from '@react-three/fiber'
+import { Canvas, useFrame, extend, useThree } from '@react-three/fiber'
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 import './App.css';
+
+extend({ OrbitControls });
+
+const CameraControls = () => {
+  // Get a reference to the three.js Camera, and the canvas html element.
+  const {
+    camera,
+    gl: { domElement },
+  } = useThree();
+  // Ref to the controls, so that we can update them on every frame using useFrame
+  const controls = useRef();
+  useFrame((state) => controls.current.update());
+  return <orbitControls ref={controls} args={[camera, domElement]} />;
+};
+
 
 function Box(props) {
   const ref = useRef()
@@ -25,6 +41,7 @@ function Box(props) {
 function App() {
   return (
     <Canvas>
+      <CameraControls />
       <ambientLight intensity={0.5} />
       <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
       <pointLight position={[-10, -10, -10]} />
