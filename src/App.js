d731d870bb4c3523ce9fd786b61338a5f5337feb
Brian Yuen <byuen@pandora.com>
2022-07-05 11:04:15 -0700

    add skybox image

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import { useRef, useState } from 'react'
-import { Canvas, useFrame, useLoader } from '@react-three/fiber'
+import { Suspense, useRef, useState } from 'react'
+import { Canvas, useFrame, useLoader, useThree } from '@react-three/fiber'
 import { Stats, OrbitControls, Billboard, Stars, Text } from '@react-three/drei'
+import * as THREE from 'three'
 import { TextureLoader } from 'three/src/loaders/TextureLoader.js'
 
 import './App.css';
+
+function Dome() {
+  const texture = useLoader(THREE.TextureLoader, 'img/nebula.jpg')
+  return (
+    <mesh>
+      <sphereBufferGeometry attach="geometry" args={[800, 100, 100]} />
+      <meshBasicMaterial attach="material" map={texture} side={THREE.BackSide} />
+    </mesh>
+  )
+}
 
 function Box(props) {
   const ref = useRef()
@@ -31,19 +42,23 @@ function App() {
     <Canvas>
       <OrbitControls />
       <ambientLight intensity={0.5} />
-      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-      <pointLight position={[-10, -10, -10]} />
-      <Stars radius={100} depth={50} count={2000} factor={8} saturation={0} fade speed={2} />
 
       <group position={[1, 3, 0]}>
-        <Box />
+        <Suspense fallback={null}>
+          <Box />
+        </Suspense>
         <Billboard follow='true'>
           <Text fontSize={0.2} outlineWidth={'5%'} outlineColor="#000000" outlineOpacity={1} position={[0, - 1, 0]} maxWidth="3" textAlign='center'>
-            This is a BOX! And some very long text that would go here!
+            Conan O'Brien Needs A Friend
           </Text>
         </Billboard>
       </group>
+      <Suspense fallback={null}>
+        <Dome />
+      </Suspense>
 
+      <Stars radius={80} depth={50} count={1500} factor={8} saturation={0} fade speed={3} />
+      <primitive object={new THREE.AxesHelper(5)} />
       <Stats showPanel={0} />
     </Canvas>
   );
