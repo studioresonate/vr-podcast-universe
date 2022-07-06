f231015608df2a24f14e3ae9bf300d76708a11d7
Brian Yuen <byuen@pandora.com>
2022-07-06 09:54:22 -0700

    add rotation to cube only

--- src/components/Cube.js
+++ src/components/Cube.js
@@ -1,5 +1,6 @@
 import React, { useMemo, useState } from 'react'
 import { TextureLoader } from 'three'
+import { useFrame } from '@react-three/fiber'
 import { Billboard, Text } from '@react-three/drei'
 
 function Box({ setRef, position, textureURL, title }) {
@@ -8,37 +9,45 @@ function Box({ setRef, position, textureURL, title }) {
     textureURL
   ])
 
+  // Maybe not a good idea for performance reasons??
+  useFrame(() => (setRef.current.rotation.y += 0.01))
 
   return (
-    <mesh
-      ref={setRef}
-      position={position}
-      onPointerOver={(event) => hover(true)}
-      onPointerOut={(event) => hover(false)} castShadow
-    >
-      <boxGeometry attach='geometry' args={[1, 1, 1]} />
-      <meshStandardMaterial
-        attach='material'
-        roughness={0.25}
-        metalness={0}
-        map={boxTexture}
-        color={hovered ? 'hotpink' : 'white'}
-      />
-      <Billboard follow='true'>
-        <Text
-          fontSize={0.2}
-          outlineWidth={'5%'}
-          outlineColor="#000000"
-          outlineOpacity={1}
-          position={[0, - 1, 0]}
-          maxWidth="3"
-          textAlign='center'
-          color='white'
+    <>
+      <group position={position}>
+        <mesh
+          ref={setRef}
+          onPointerOver={(event) => hover(true)}
+          onPointerOut={(event) => hover(false)} castShadow
         >
-          {title}
-        </Text>
-      </Billboard>
-    </mesh>
+          <boxGeometry attach='geometry' args={[1, 1, 1]} />
+          <meshStandardMaterial
+            attach='material'
+            roughness={0.25}
+            metalness={0}
+            map={boxTexture}
+            color={hovered ? 'hotpink' : 'white'}
+          />
+        </mesh>
+        <mesh
+          position={[0, - 1, 0]}
+        >
+          <Billboard follow='true'>
+            <Text
+              fontSize={0.2}
+              outlineWidth={'5%'}
+              outlineColor="#000000"
+              outlineOpacity={1}
+              maxWidth="3"
+              textAlign='center'
+              color='white'
+            >
+              {title}
+            </Text>
+          </Billboard>
+        </mesh>
+      </group>
+    </>
   )
 }
 
