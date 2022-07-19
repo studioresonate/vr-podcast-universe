3de948b463890580ca332e859ba989728f308bac
Brian Yuen <brianyuen@outlook.com>
2022-07-19 14:36:32 -0700

    add texture to sxm

--- src/components/Sun.js
+++ src/components/Sun.js
@@ -1,35 +1,63 @@
-import React, { useRef, useState } from 'react'
-// import { TextureLoader } from 'three'
-// import { useFrame } from '@react-three/fiber'
+import React, { Suspense, useRef, useState } from 'react'
+import * as THREE from "three";
+import { useFrame, useLoader } from '@react-three/fiber'
+
 
 function Sun() {
   const [hovered, hover] = useState(false)
-  // const SunTexture = useMemo(() => new TextureLoader().load(textureURL), [
-  //   textureURL
-  // ])
 
-  // Maybe not a good idea for performance reasons??
+  const [colorMap, normalMap] = useLoader(THREE.TextureLoader, [
+    './img/textures/texture-sxm.png',
+    './img/textures/texture-sxm-normal.png'
+  ])
+
+  const [cloudMap] = useLoader(THREE.TextureLoader, [
+    './img/textures/clouds.png'
+  ])
+
+
+
   const sunRef = useRef()
+  const ref = useRef()
+  useFrame(() => (ref.current.rotation.y += 0.001))
 
 
   return (
     <>
-      <mesh
-        ref={sunRef}
-        onPointerOver={(event) => hover(true)}
-        onPointerOut={(event) => hover(false)}
+      <Suspense fallback={null}>
+        <mesh ref={ref}>
+          <sphereBufferGeometry
+            args={[2.02, 30, 30]}
+            attach="geometry"
+          />
+          <meshPhongMaterial
+            attach="material"
+            map={cloudMap}
+            transparent
+          />
+        </mesh>
 
-      >
-        <sphereBufferGeometry
-          args={[2, 15, 8]}
-          attach="geometry"
-        />
-        <meshStandardMaterial
-          attach="material"
-          color={hovered ? 'hotpink' : 'white'}
-          wireframe
-        />
-      </mesh>
+        <mesh
+          ref={sunRef}
+          onPointerOver={(event) => hover(true)}
+          onPointerOut={(event) => hover(false)}
+
+        >
+          <sphereBufferGeometry
+            args={[2, 20, 30]}
+            attach="geometry"
+          />
+          <meshPhongMaterial
+            attach="material"
+            color={hovered ? 'hotpink' : 'white'}
+            map={colorMap}
+            normalMap={normalMap}
+            metalness={1}
+            shininess={2}
+            flatShading={false}
+          />
+        </mesh>
+      </Suspense>
 
     </>
   )
