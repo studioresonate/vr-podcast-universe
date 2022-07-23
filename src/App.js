6479be9ed7e84de2f760245467cd27842c3e63a4
Brian Yuen <brianyuen@outlook.com>
2022-07-22 22:03:14 -0700

    fix text lookAt. Temp move everything to App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,145 @@
-import { DefaultXRControllers, VRCanvas } from '@react-three/xr'
-import { Stats, OrbitControls } from '@react-three/drei'
-import * as THREE from 'three'
+import { Suspense, useEffect, useMemo, useRef, useState } from 'react'
+import { DefaultXRControllers, VRCanvas, useXR } from '@react-three/xr'
+import { useFrame, useThree } from '@react-three/fiber'
+import { Stats, OrbitControls, useCursor, Text } from '@react-three/drei'
+// import * as THREE from 'three'
+import { TextureLoader } from 'three'
 
-// import query from './components/Query'
 import './App.css';
 
-import Cubes from './components/Podcast';
+// import Cubes from './components/Podcast';
 import Space from './components/Space';
 import Sun from './components/Sun';
 import XR from './util/Xr'
+
+import query from './components/Query'
+
+
+// Generate random number exept -1 thru 1, since that's where the sun is occupied
+function generateRandom(min, max) {
+  const num = Math.random() * (max - min + 1) + min;
+  return (num >= -1 && num <= 1) ? generateRandom(min, max) : num;
+}
+
+// Cubes
+function Cubes() {
+  const [podcastCollection, setpodcastCollection] = useState([]);
+  const cubez = useRef()
+
+  useFrame(() => (cubez.current.rotation.y += 0.002))
+
+  useEffect(() => {
+    fetch(
+      `https://graphql.contentful.com/content/v1/spaces/${process.env.REACT_APP_SPACE_ID}/`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          // Authenticate the request
+          Authorization: `Bearer ${process.env.REACT_APP_CONTENT_DELIVERY}`,
+        },
+        // send the GraphQL query
+        body: JSON.stringify({ query }),
+      }
+    )
+      .then((response) => response.json())
+      .then(({ data, errors }) => {
+        if (errors) {
+          console.error(errors);
+        }
+        // rerender the entire component with new data
+        setpodcastCollection(data.podcastCollection.items);
+        // console.log(data.podcastCollection.items);
+      });
+  }, []);
+
+  if (!podcastCollection) {
+    return "Loading...";
+  }
+
+  return (
+    <group ref={cubez}>
+      <Suspense fallback={null}>
+        {podcastCollection.map(podcast => (
+          <Cube
+            key={podcast.sys.id}
+            position={[
+              generateRandom(-10, 10),
+              generateRandom(-10, 10),
+              generateRandom(-10, 10)
+            ]}
+            textureURL={`${podcast.coverArt.url}?fit=scale&w=300&h=300&q=70`}
+            title={podcast.podcastTitle}
+          />
+        ))}
+      </Suspense>
+    </group>
+  );
+}
+
+// Cube
+function Cube({ position, textureURL, title }) {
+  const [hovered, hover] = useState(false)
+  const boxTexture = useMemo(() => new TextureLoader().load(textureURL), [
+    textureURL
+  ])
+
+  useCursor(hovered, /*'pointer', 'auto'*/)
+
+  const ref = useRef()
+  useFrame(() => (ref.current.rotation.y += 0.02))
+
+  // Text lookAt
+  const { player } = useXR();
+  const textRef = useRef()
+  const { camera } = useThree()
+  // Only works for when isPresenting is true
+  useFrame(() => textRef.current.lookAt(player.position))
+  // Desktop view
+  useFrame(() => textRef.current.lookAt(camera.position))
+
+  return (
+    <>
+      <group position={position}>
+        <mesh
+          ref={ref}
+          onPointerOver={() => hover(true)}
+          onPointerOut={() => hover(false)} castShadow
+          onClick={() => {
+            console.log('Clicked')
+          }}
+        >
+          <boxGeometry attach='geometry' args={[1, 1, 1]} />
+          <meshStandardMaterial
+            attach='material'
+            roughness={0.25}
+            metalness={0.8}
+            map={boxTexture}
+            color={hovered ? 'hotpink' : 'white'}
+          />
+        </mesh>
+
+        <mesh position={[0, - 1, 0]}>
+          <Text
+            fontSize={0.2}
+            outlineWidth={'5%'}
+            outlineColor="#000000"
+            outlineOpacity={1}
+            maxWidth="3"
+            textAlign='center'
+            color='white'
+            ref={textRef}
+          >
+            {title}
+          </Text>
+        </mesh>
+      </group>
+    </>
+  )
+}
+
+
+
 
 function App() {
   return (
@@ -36,7 +167,7 @@ function App() {
       <XR />
 
       {/* debug */}
-      <primitive object={new THREE.AxesHelper(5)} />
+      {/* <primitive object={new THREE.AxesHelper(5)} /> */}
       <Stats showPanel={0} />
     </VRCanvas>
   );
