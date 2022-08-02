c111edb91dd3c689954ebf37b0f589f460fd5b1e
Brian Yuen <brianyuen@outlook.com>
2022-08-01 21:09:13 -0700

    podcast modal

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { Suspense, useEffect, useMemo, useRef, useState } from 'react'
 import { VRCanvas } from '@react-three/xr'
 import { useFrame, useThree } from '@react-three/fiber'
-import { Stats, useCursor, Text } from '@react-three/drei'
-// import * as THREE from 'three'
+import { Stats, useCursor, useAspect, Text } from '@react-three/drei'
+import * as THREE from 'three'
 import { TextureLoader } from 'three'
 import { Control } from './util/VRControl'
 
@@ -155,6 +155,128 @@ document.body.insertAdjacentHTML('afterbegin', `
 `)
 
 
+function PodcastModal() {
+  const size = useAspect(1024, 512)
+  const [video] = useState(() =>
+    Object.assign(document.createElement('video'), { src: 'https://videos.ctfassets.net/ydi6vzm9xood/4FKwgfyNBD8Z6FLZG0vhmJ/10e5ee7d33562e05548a8f6c9b0ab9ea/StarTalk.mp4', crossOrigin: 'Anonymous', loop: false, muted: true }),
+  )
+  video.muted = "muted"
+  useEffect(() => void video.play(), [video])
+
+  return (
+    <>
+      <group position={[0, 2, 4]}>
+        <Text
+          fontSize={0.8}
+          outlineWidth={'5%'}
+          outlineColor="#000000"
+          outlineOpacity={1}
+          maxWidth="10"
+          textAlign='left'
+          color='white'
+          position={[-3.6, -4.2, 0]}
+          anchorX="left"
+          anchorY="top"
+        // ref={textRef}
+        >
+          StarTalk this is a super long headline
+        </Text>
+
+        {/* video */}
+        <mesh scale={size}>
+          <planeBufferGeometry args={[0.3, 0.3]} />
+          <meshBasicMaterial toneMapped={false} side={THREE.DoubleSide}>
+            <videoTexture attach="map" args={[video]} encoding={THREE.sRGBEncoding} />
+          </meshBasicMaterial>
+        </mesh>
+
+        {/* play button */}
+        <mesh position={[0.1, 0, 0.1]} rotation={[1.6, 0, 1.6]}>
+          <capsuleGeometry args={[0.7, 3, 4, 2]} />
+          <Text
+            fontSize={0.7}
+            // outlineWidth={'5%'}
+            // outlineColor="#000000"
+
+            outlineOpacity={1}
+            maxWidth="10"
+            textAlign='center'
+            color='black'
+            rotation={[180, 1.6, 0.64]}
+            position={[0.1, 0, 0]}
+          // ref={textRef}
+          >
+            PLAY
+          </Text>
+          <meshBasicMaterial color={0xff000} />
+        </mesh>
+
+        {/* close button */}
+        <mesh position={[6.8, 3.3, 0.1]}>
+          <circleGeometry args={[0.5, 32]} />
+          <meshBasicMaterial color={0xf3f3f3} />
+        </mesh>
+
+        {/* thumbnail */}
+        <mesh position={[-5.7, -5.3, 0]}>
+          <planeBufferGeometry args={[2.4, 2.4]} color={0x00ff00} />
+          <meshBasicMaterial />
+        </mesh>
+      </group>
+
+      <mesh rotation={[0, -0.5, 0]}>
+        <group position={[8.5, 4, 0]}>
+          <Text
+            fontSize={0.4}
+            outlineWidth={'5%'}
+            outlineColor="#000000"
+            outlineOpacity={1}
+            maxWidth="10"
+            textAlign='left'
+            color='white'
+            anchorX="left"
+          >
+            HOSTS
+          </Text>
+        </group>
+        <group position={[8.5, 3.4, 0]}>
+          <Text
+            fontSize={0.3}
+            outlineWidth={'5%'}
+            outlineColor="#000000"
+            outlineOpacity={1}
+            maxWidth="10"
+            textAlign='left'
+            color='white'
+            anchorX="left"
+          >
+            Neil deGrasse Tyson, Chuck Nice
+          </Text>
+        </group>
+        <group position={[8.5, 2.2, 0]}>
+          <Text
+            fontSize={0.2}
+            outlineWidth={'5%'}
+            outlineColor="#000000"
+            outlineOpacity={1}
+            maxWidth="6"
+            textAlign='left'
+            color='white'
+            anchorX="left"
+            anchorY="top"
+            lineHeight={2}
+          >
+            Congue nullam lacinia habitasse rhoncus eu finibus pulvinar, ornare aliquam ante nam diam sem, mattis ullamcorper netus venenatis platea massa. Elementum lobortis diam nisi finibus integer vestibulum dictum accumsan, curae fusce ridiculus habitasse justo sit rhoncus eros ut, montes facilisi duis massa praesent senectus ante. Et malesuada ipsum elementum tempor augue nibh orci montes, pellentesque penatibus dis metus lacinia nunc porta inceptos, tempus ridiculus sagittis enim conubia parturient proin. Maximus pretium dictum curae integer hendrerit porttitor rutrum turpis vestibulum vehicula, accumsan nunc mauris porta etiam ultrices odio sed per proin
+          </Text>
+        </group>
+      </mesh>
+
+
+    </>
+  )
+}
+
+
 
 
 function App() {
@@ -180,6 +302,7 @@ function App() {
       <Sun />
       <Space />
       <Control />
+      <PodcastModal />
 
       {/* <XR /> */}
 
