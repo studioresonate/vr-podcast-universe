35d1a2173abaaf4c0b7aed7a49462588d970f10f
Brian Yuen <brianyuen@outlook.com>
2022-08-03 16:03:11 -0700

    put modal in component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import { Suspense, useEffect, useMemo, useRef, useState } from 'react'
 import { VRCanvas } from '@react-three/xr'
 import { useFrame, useThree } from '@react-three/fiber'
-import { Stats, useCursor, useAspect, Text } from '@react-three/drei'
-import * as THREE from 'three'
+import { Stats, useCursor, Text } from '@react-three/drei'
 import { TextureLoader } from 'three'
 import { Control } from './util/VRControl'
 
-
+import PodcastModal from './components/Podcast/Modal'
 
 import './App.css';
 
@@ -107,8 +106,6 @@ function Cube({ position, textureURL, title }) {
   // Desktop view
   useFrame(() => textRef.current.lookAt(camera.position))
 
-  console.log(camera.position);
-
   return (
     <>
       <group position={position}>
@@ -150,137 +147,12 @@ function Cube({ position, textureURL, title }) {
 }
 
 
-document.body.insertAdjacentHTML('afterbegin', `
-    <p class="instructions">Move with <span class="circle">L</span> thumbstick in VR or <span>W</span>, <span>A</span>, <span>S</span>, and <span>D</span> keys. Rotate with <span class="circle">R</span> thumbstick in VR or arrow keys. &larr; &uarr; &rarr;	&darr;</p>
-`)
-
-
-function PodcastModal() {
-  const size = useAspect(1024, 512)
-  const [video] = useState(() =>
-    Object.assign(document.createElement('video'), { src: 'https://videos.ctfassets.net/ydi6vzm9xood/4FKwgfyNBD8Z6FLZG0vhmJ/10e5ee7d33562e05548a8f6c9b0ab9ea/StarTalk.mp4', crossOrigin: 'Anonymous', loop: false, muted: true }),
-  )
-  video.muted = "muted"
-  useEffect(() => void video.play(), [video])
-
-  return (
-    <>
-      <group position={[0, 2, 4]}>
-        <Text
-          fontSize={0.8}
-          outlineWidth={'5%'}
-          outlineColor="#000000"
-          outlineOpacity={1}
-          maxWidth="10"
-          textAlign='left'
-          color='white'
-          position={[-3.6, -4.2, 0]}
-          anchorX="left"
-          anchorY="top"
-        // ref={textRef}
-        >
-          StarTalk this is a super long headline
-        </Text>
-
-        {/* video */}
-        <mesh scale={size}>
-          <planeBufferGeometry args={[0.3, 0.3]} />
-          <meshBasicMaterial toneMapped={false} side={THREE.DoubleSide}>
-            <videoTexture attach="map" args={[video]} encoding={THREE.sRGBEncoding} />
-          </meshBasicMaterial>
-        </mesh>
-
-        {/* play button */}
-        <mesh position={[0.1, 0, 0.1]} rotation={[0, 0, 1.57]}>
-          <planeGeometry args={[1, 3.6]} />
-          <Text
-            fontSize={0.5}
-            // outlineWidth={'5%'}
-            // outlineColor="#000000"
-
-            outlineOpacity={1}
-            maxWidth="10"
-            textAlign='center'
-            color='white'
-            // rotation={[3.8, 0.1, 0.64]}
-            rotation={[0, 0, -1.6]}
-            position={[0, 0, 0.01]}
-          // ref={textRef}
-          >
-            PLAY
-          </Text>
-          <meshBasicMaterial color={0x0051e6} />
-        </mesh>
-
-        {/* close button */}
-        <mesh position={[6.8, 3.3, 0.1]}>
-          <circleGeometry args={[0.5, 32]} />
-          <meshBasicMaterial color={0xf3f3f3} />
-        </mesh>
-
-        {/* thumbnail */}
-        <mesh position={[-5.7, -5.3, 0]}>
-          <planeBufferGeometry args={[2.4, 2.4]} color={0x00ff00} />
-          <meshBasicMaterial />
-        </mesh>
-      </group>
-
-      <mesh rotation={[0, -0.5, 0]}>
-        <group position={[8.5, 4, 0]}>
-          <Text
-            fontSize={0.4}
-            outlineWidth={'5%'}
-            outlineColor="#000000"
-            outlineOpacity={1}
-            maxWidth="10"
-            textAlign='left'
-            color='white'
-            anchorX="left"
-          >
-            HOSTS
-          </Text>
-        </group>
-        <group position={[8.5, 3.4, 0]}>
-          <Text
-            fontSize={0.3}
-            outlineWidth={'5%'}
-            outlineColor="#000000"
-            outlineOpacity={1}
-            maxWidth="10"
-            textAlign='left'
-            color='white'
-            anchorX="left"
-          >
-            Neil deGrasse Tyson, Chuck Nice
-          </Text>
-        </group>
-        <group position={[8.5, 2.2, 0]}>
-          <Text
-            fontSize={0.2}
-            outlineWidth={'5%'}
-            outlineColor="#000000"
-            outlineOpacity={1}
-            maxWidth="6"
-            textAlign='left'
-            color='white'
-            anchorX="left"
-            anchorY="top"
-            lineHeight={2}
-          >
-            Congue nullam lacinia habitasse rhoncus eu finibus pulvinar, ornare aliquam ante nam diam sem, mattis ullamcorper netus venenatis platea massa. Elementum lobortis diam nisi finibus integer vestibulum dictum accumsan, curae fusce ridiculus habitasse justo sit rhoncus eros ut, montes facilisi duis massa praesent senectus ante. Et malesuada ipsum elementum tempor augue nibh orci montes, pellentesque penatibus dis metus lacinia nunc porta inceptos, tempus ridiculus sagittis enim conubia parturient proin. Maximus pretium dictum curae integer hendrerit porttitor rutrum turpis vestibulum vehicula, accumsan nunc mauris porta etiam ultrices odio sed per proin
-          </Text>
-        </group>
-      </mesh>
-
-
-    </>
-  )
-}
-
-
-
-
 function App() {
+  useEffect(() => {
+    document.body.insertAdjacentHTML('afterbegin', `
+        <p class="instructions">Move with <span class="circle">L</span> thumbstick in VR or <span>W</span>, <span>A</span>, <span>S</span>, and <span>D</span> keys. Rotate with <span class="circle">R</span> thumbstick in VR or arrow keys. &larr; &uarr; &rarr;	&darr;</p>
+    `)
+  })
   return (
     <VRCanvas vr="true" camera={{ position: [0, 0, 15] }}  >
       {/* <DefaultXRControllers
