2ab6cc8bad81c9af443a6525bd97130cee42eea7
Brian Yuen <brianyuen@outlook.com>
2022-08-06 14:47:52 -0700

    fix className

--- src/components/instructions.js
+++ src/components/instructions.js
@@ -1,7 +1,7 @@
 export default function instructions() {
   return (
     <>
-      <p class="instructions">Move with <span class="circle">L</span> thumbstick in VR or <span>W</span>, <span>A</span>, <span>S</span>, and <span>D</span> keys. Rotate with <span class="circle">R</span> thumbstick in VR or arrow keys. &larr; &uarr; &rarr;	&darr;</p>
+      <p className="instructions">Move with <span className="circle">L</span> thumbstick in VR or <span>W</span>, <span>A</span>, <span>S</span>, and <span>D</span> keys. Rotate with <span className="circle">R</span> thumbstick in VR or arrow keys. &larr; &uarr; &rarr;	&darr;</p>
     </>
   )
 }
