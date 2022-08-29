33d339d420ec21110ebead13120f6aa4561a4bcb
Brian Yuen <brianyuen@outlook.com>
2022-08-29 14:06:26 -0700

    animation from intro screen

--- src/util/VRControl.js
+++ src/util/VRControl.js
@@ -1,6 +1,8 @@
 import { useFrame, useThree } from '@react-three/fiber'
 import ImmersiveControls from '@depasquale/three-immersive-controls';
 import * as THREE from 'three'
+import { gsap } from "gsap";
+
 
 export const Control = () => {
   const { camera, gl, scene } = useThree()
@@ -11,6 +13,14 @@ export const Control = () => {
     floor: false,
     showExitVRButton: false,
     showEnterVRButton: false
+  });
+
+  const tl = gsap.timeline();
+  tl.to(controls.player.position, {
+    duration: 6,
+    delay: 1,
+    z: 17,
+    ease: "power1.inOut"
   });
 
   useFrame(() => (controls.update()))
