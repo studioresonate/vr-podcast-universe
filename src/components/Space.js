24cdbfc648ed5004dfa484b2028787d293b2e37c
Brian Yuen <brianyuen@outlook.com>
2022-08-30 00:08:12 -0700

    load gltf model

--- src/components/Space.js
+++ src/components/Space.js
@@ -1,12 +1,20 @@
 import * as THREE from 'three'
 import { Suspense } from 'react'
 import { useLoader } from '@react-three/fiber'
-import { Sparkles, Stars } from '@react-three/drei'
+import { Sparkles, Stars, useGLTF } from '@react-three/drei'
+
+function RocketModel() {
+  const gltf = useGLTF('/models/rocket.gltf')
+  return (<primitive object={gltf.scene} />)
+}
 
 function Dome() {
   const texture = useLoader(THREE.TextureLoader, 'img/nebula.jpg')
   return (
     <Suspense fallback={null}>
+      <group position={[0, 0, 300]} rotation={[0, 20, 0]}>
+        <RocketModel />
+      </group>
       <mesh>
         <sphereBufferGeometry attach="geometry" args={[800, 100, 100]} />
         <meshBasicMaterial attach="material" map={texture} side={THREE.BackSide} />
