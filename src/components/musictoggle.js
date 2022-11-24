871edf288b96dff9f9ef14f5f51b1fd5e2c0e9fd
Brian Yuen <brianyuen@outlook.com>
2022-11-24 00:22:32 -0800

    vr music toggle

---
+++ src/components/musictoggle.js
@@ -0,0 +1,26 @@
+AFRAME.registerComponent('musictoggle', {
+
+  init: function () {
+    const el = this.el;
+    const toggle = document.querySelector('.toggle')
+
+    function vrMusicToggle() {
+      let sun = document.querySelector('#sun')
+      if (sun.hasAttribute('music')) {
+        sun.setAttribute('mixin', 'bgmusic-on');
+        sun.removeAttribute('music');
+        el.setAttribute('value', "MUSIC ON")
+        toggle.setAttribute('animation', 'property: position; to: 1.3 0 -0.002; dur: 200; easing: linear; loop: false')
+        toggle.setAttribute('animation__1', 'property: components.material.material.color; type: color; to: #0051e6; dur: 200')
+      } else {
+        sun.setAttribute('mixin', 'bgmusic-off');
+        sun.setAttribute('music', '1');
+        el.setAttribute('value', "MUSIC OFF")
+        toggle.setAttribute('animation', 'property: position; to: -0.2 0 -0.002; dur: 200; easing: linear; loop: false')
+        toggle.setAttribute('animation__1', 'property: components.material.material.color; type: color; to: #383838; dur: 200')
+      }
+    };
+
+    el.addEventListener('click', vrMusicToggle);
+  }
+});
