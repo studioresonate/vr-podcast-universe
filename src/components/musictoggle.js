9e25524e67c490135215b5f4b803545d271a2bc1
Brian Yuen <brianyuen@outlook.com>
2022-12-01 15:57:56 -0800

    move components into comp folder, fix music toggle

--- src/components/musictoggle.js
+++ src/components/musictoggle.js
@@ -10,13 +10,13 @@ AFRAME.registerComponent('musictoggle', {
         sun.setAttribute('mixin', 'bgmusic-on');
         sun.removeAttribute('music');
         el.setAttribute('value', "MUSIC ON")
-        toggle.setAttribute('animation', 'property: position; to: 1.3 0 -0.002; dur: 200; easing: linear; loop: false')
+        toggle.setAttribute('animation', 'property: position; to: 0.085 0 0; dur: 200; easing: linear; loop: false')
         toggle.setAttribute('animation__1', 'property: components.material.material.color; type: color; to: #0051e6; dur: 200')
       } else {
         sun.setAttribute('mixin', 'bgmusic-off');
         sun.setAttribute('music', '1');
         el.setAttribute('value', "MUSIC OFF")
-        toggle.setAttribute('animation', 'property: position; to: -0.2 0 -0.002; dur: 200; easing: linear; loop: false')
+        toggle.setAttribute('animation', 'property: position; to: -0.018 0 0; dur: 200; easing: linear; loop: false')
         toggle.setAttribute('animation__1', 'property: components.material.material.color; type: color; to: #383838; dur: 200')
       }
     };
