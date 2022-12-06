3a4f7a5f6880045b214a6481ba1dcb5c292a49a9
Brian Yuen <brianyuen@outlook.com>
2022-12-05 16:47:01 -0800

    tweak close modal animation timing

--- src/components/modal.js
+++ src/components/modal.js
@@ -14,11 +14,11 @@ AFRAME.registerComponent("closemodal", {
       modalvideo.setAttribute('src', '')
       modalvideo.setAttribute('visible', false)
       // podcastmodal.remove()
-      podcastmodal.setAttribute('animation', 'property: scale; from: 1 1 1; to: 0.5 0 0.5; loop: false; easing:easeInCubic; dur: 90')
+      podcastmodal.setAttribute('animation', 'property: scale; from: 1 1 1; to: 0.5 0 0.5; loop: false; easing:easeInCubic; dur: 200')
       setTimeout(() => {
-        modalBg.setAttribute('animation', 'property: scale; from: 1 1 1; to: 0.6 0.6 0.6; loop: false; easing:easeOutExpo; dur: 200')
-        modalBg.setAttribute('animation__1', 'property: opacity; from: 0.4; to: 0; loop: false; easing:easeOutExpo; dur: 200')
-        modalBg.setAttribute('animation__2', 'property: rotation; from: 0 0 0; to: 0 -90 0; loop: false; easing:easeOutExpo; dur: 150')
+        modalBg.setAttribute('animation', 'property: scale; from: 1 1 1; to: 0.6 0.6 0.6; loop: false; easing:easeOutExpo; dur: 300')
+        modalBg.setAttribute('animation__1', 'property: opacity; from: 0.4; to: 0; loop: false; easing:easeOutExpo; dur: 300')
+        modalBg.setAttribute('animation__2', 'property: rotation; from: 0 0 0; to: 0 -90 0; loop: false; easing:easeOutExpo; dur: 350')
         podcastmodal.setAttribute('position', '0 300 -3')
       }, 100);
     })
