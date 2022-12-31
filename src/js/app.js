716519ced2b49cb9acbd3a7bba7aa85df9d01875
Brian Yuen <brianyuen@outlook.com>
2022-12-31 00:34:30 -0800

    fix chrome dev, consolidate loading

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,15 @@
+window.document.body.insertAdjacentHTML('afterbegin', `
+<div id="loading"><span>Loading</span></div>
+`)
+
 window.addEventListener('load', () => {
+  const loadTime = window.performance.timing.loadEventStart - window.performance.timing.navigationStart;
+  console.log(`⚡ Huzzah! Page loaded in ${loadTime}ms`);
   setTimeout(() => {
     document.querySelector('#loading').classList.add('loaded')
   }, 800);
 })
 
-// const scene = document.querySelector('a-scene');
-// scene.addEventListener('loaded', () => {
-//   setTimeout(() => {
-//     document.querySelector('#loading').classList.add('loaded')
-//   }, 800);
-// });
 
 const text = `
 ╔═╗╔╦╗╦ ╦╔╦╗╦╔═╗  ╦═╗╔═╗╔═╗╔═╗╔╗╔╔═╗╔╦╗╔═╗
@@ -21,8 +21,3 @@ https://studioresonate.com
   ;
 const style = "color: #23d1ba";
 console.log(`%c${text}`, style);
-
-window.onload = function () {
-  const loadTime = window.performance.timing.loadEventStart - window.performance.timing.navigationStart;
-  console.log(`⚡ Huzzah! Page loaded in ${loadTime}ms`);
-}
